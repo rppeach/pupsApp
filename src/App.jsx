b481a01d87d6b23b1d5ce1ec7e26ba390058bbc2
ee-rpeach <157056610+ee-rpeach@users.noreply.github.com>
2025-03-26 23:24:33 -0400

    completed workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import './App.css'
-import {puppylist} from './data.js'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
 
-console.log(puppylist)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log("puppyList: ", puppyList);
 
   return (
-    <div>
-      
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default App
